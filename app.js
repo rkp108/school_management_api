81624165cf9bae7afc662cad4d562b76751e00e1
rkp108 <108rahulkp@gmail.com>
2024-08-29 12:48:37 +0530

    first commit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const schoolRoutes = require('./routes/schoolRoutes');
 const app = express();
 
 app.use(bodyParser.json());
-// app.use('/api', schoolRoutes);
-app.get("/", (req, res) =>{ return res.send("sdwd")})
+app.use('/api', schoolRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
